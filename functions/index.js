cf1f9b50d01f2f54812a98d217b0ae7c5a733837
Sam <anan1411@gmail.com>
2017-12-13 14:57:38 -0700

    implement database writing/retriving

--- functions/index.js
+++ functions/index.js
@@ -1,36 +1,37 @@
 'use strict';
 
+// Actions on Google
 const app = require('actions-on-google').DialogflowApp;
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 const sprintf = require('sprintf-js').sprintf;
-
 admin.initializeApp(functions.config().firebase);
-const db = admin.database();
+
+const db = admin.database().ref('/perfect_number');
+const storage = admin.storage()
 
 class PerfectNumber {
     constructor(req, res) {
         this.sessionId = req.body.sessionId;
         console.log(sprintf("Headers: %s", JSON.stringify(req.headers)));
         console.log(sprintf("Body: %s", JSON.stringify(req.body)));
-        this.app = new app({ request: req, response: res });
-        console.log(sprintf("User: %s", JSON.stringify(this.app.getUser)));
-        console.log(sprintf("Data: %s", JSON.stringify(this.app.data)));
+        this.assistant = new app({ request: req, response: res });
+        console.log(sprintf("User: %s", JSON.stringify(this.assistant.getUser)));
+        console.log(sprintf("Data: %s", JSON.stringify(this.assistant.data)));
     }
 
     run() {
-        const action = this.app.getIntent();
-        console.log(action);
-        const ref = db.ref('perfect_number/'+this.sessionId);
-        ref.once('value').then(
-            function(snapshot) {
-                console.log(sprintf("Snapshot: %s", JSON.stringify(snapshot)));
-            }
-        )
-        ref.set({
-            Data: JSON.stringify(this.app.data)
+        const action = this.assistant.getIntent();
+        const ref = db.child(this.sessionId);
+        // Get data from /perfect_number/[sessionId]
+        let prevData = null
+        ref.once('value', snap => {
+            prevData = snap.val();
+            // Set data at /perfect_number/[sessionId]
+            ref.set({Data: JSON.stringify(this.assistant.data)})
         });
-        this.app.ask(sprintf("Previous data is %s", JSON.stringify(prev_data)));
+        console.log(`Prev Data: ${JSON.stringify(prevData)}`);
+        this.assistant.ask(`Data retrival/write successful\nPrev Data: ${JSON.stringify(prevData)}`)
     }
 }
 
